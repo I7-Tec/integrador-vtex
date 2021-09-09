9c7c4ac17e8835a18e094dae95f2db5256ee0118
harrysonoliveira <harryoga@hotmail.com>
2021-09-09 17:26:35 -0300

    update catalog

--- src/main/java/br/com/i7solution/vtex/apivtex/CatalogClient.java
+++ src/main/java/br/com/i7solution/vtex/apivtex/CatalogClient.java
@@ -1,12 +1,14 @@
 package br.com.i7solution.vtex.apivtex;
 
 import br.com.i7solution.vtex.apivtex.dtos.*;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import kong.unirest.UnirestException;
 
+@Log4j2
 @Service
 public class CatalogClient {
 
@@ -194,6 +196,7 @@ public class CatalogClient {
                     .header("X-VTEX-API-AppToken", DadosVtex.appToken)
                     .body(dados)
                     .asObject(ProductInclusaoDTO.class);
+                    log.info(response.getStatus());
             return response.getBody();
         } catch (UnirestException e) {
             e.printStackTrace();
